2ba0d0ab63248fc28559046381052dfb4a28c0e5
Tamas Foldes <foldes.andrei@gmail.com>
2025-11-04 13:49:58 +0000

    chore: update Playwright configuration for improved testing environment
    This commit modifies the Playwright configuration to enhance the testing setup. Key changes include:
    
    - Updated the base URL to use an environment variable for flexibility, defaulting to 'http://localhost:8080'.
    - Re-enabled the local web server configuration with a specified command, URL, and timeout settings.
    
    These adjustments aim to streamline the testing process and improve the development workflow.

--- playwright.config.js
+++ playwright.config.js
@@ -19,14 +19,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://andreifoldes.github.io/timediary',
-    
+    baseURL: process.env.TEST_URL || 'http://localhost:8080',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
+
     /* Take screenshot on failure */
     screenshot: 'only-on-failure',
-    
+
     /* Record video on failure */
     video: 'retain-on-failure',
   },
@@ -70,9 +70,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:8080',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:8080',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 }); 
